8bfdbc1c86d5ca02b70c423b51c62db734b68c86
MiguelDG-96 <mdolicg@gmail.com>
2025-12-13 21:26:02 -0500

    Auto-generated commit

--- chunk-UULUGAA5.js
+++
@@ -1,1 +0,0 @@
-import{Kb as i,_a as n,sb as s,tb as o}from"./chunk-7BIZPBS4.js";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-settings"]],decls:2,vars:0,template:function(e,m){e&1&&(s(0,"p"),i(1,"settings works!"),o())},encapsulation:2})};export{a as SettingsComponent};
